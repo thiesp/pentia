8b2bba05c8b9992fac2c51c331af6265c0348694
Thies Pierdola <tpi@pentia.dk>
2023-11-06 11:59:54 +0100

    adding products to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'faker'
+
+10.times do |n|
+  Product.create!(name: Faker::Commerce.product_name, description: Faker::Lorem.sentence, price: Faker::Commerce.price)
+end
