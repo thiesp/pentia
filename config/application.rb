1663bd81ee876f12fa4e06e5d87c95fc76494f05
Thies Pierdola <tpi@pentia.dk>
2023-11-10 10:34:53 +0100

    autoload services

--- config/application.rb
+++ config/application.rb
@@ -28,6 +28,8 @@ module Asssignment
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
 
+    config.autoload_paths << "#{Rails.root}/app/services"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
